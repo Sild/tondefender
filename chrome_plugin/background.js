8cb39e5f8decb9a07fbfbcefa8b08071b6f11ba4
Dmitrii Korchagin <d.korchagin@ston.fi>
2024-10-18 16:48:09 +0200

    use whilelist set, up plugin name

--- chrome_plugin/background.js
+++ chrome_plugin/background.js
@@ -1,22 +1,24 @@
 // List of whitelisted URLs
-const whitelist = [
-    "https://www.example.com"
-  ];
+const whitelist_domains = new Set([
+  "ston.fi"
+]);
+
+function extractDomain(url) {
+  const domain = url.match(/:\/\/(www\.)?(.[^/]+)/)[2];
+  return domain;
+}
   
-  // Listen for tab updates
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url) {
-      for (let i = 0; i < whitelist.length; i++) {
-        if (tab.url.startsWith(whitelist[i])) {
-          // Inject script to display alert
-          chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: () => {
-              alert("You have visited a whitelisted URL.");
-            }
-          });
-          break;
+// Listen for tab updates
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url) {
+    let domain = extractDomain(tab.url);
+    if (!whitelist_domains.has(domain)) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: () => {
+          alert("You have visited a whitelisted URL.");
         }
-      }
+      });
     }
-  });
+  }
+});

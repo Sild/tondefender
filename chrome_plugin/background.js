06ba83d7de9600e43ba8b803a06582330fcd8c11
Dmitrii Korchagin <d.korchagin@ston.fi>
2024-10-18 17:11:28 +0200

    try detect tonconnect

--- chrome_plugin/background.js
+++ chrome_plugin/background.js
@@ -1,24 +1,61 @@
 // List of whitelisted URLs
-const whitelist_domains = new Set([
+const ton_connect_allowed_domains = new Set([
   "ston.fi"
 ]);
 
+let ton_connect_str = new Set([
+  "ton-connect",
+  "tonconnect"
+])
+
+function site_has_ton_connect(site_body) {
+  let site_lower = site_body.toLowerCase();
+  for (let str of ton_connect_str) {
+    if (site_lower.includes(str)) {
+      return true;
+    }
+  }
+}
+
 function extractDomain(url) {
   const domain = url.match(/:\/\/(www\.)?(.[^/]+)/)[2];
-  return domain;
+  const domainParts = domain.split('.');
+  const secondLevelDomain = domainParts.slice(-2).join('.');
+  return secondLevelDomain;
+}
+
+function is_whitelisted_domain(domain) {
+  return ton_connect_allowed_domains.has(domain);
+}
+
+function alert(text) {
+  chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: () => {
+      alert(text);
+    }
+  });
 }
   
 // Listen for tab updates
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url) {
-    let domain = extractDomain(tab.url);
-    if (whitelist_domains.has(domain)) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        func: () => {
-          alert("You have visited a whitelisted URL.");
-        }
-      });
-    }
+    console.log("tabId", tabId);
+    // chrome.scripting.executeScript({
+    //   target: { tabId: tabId },
+    //   func: () => document.body.innerHTML
+    // }, (results) => {
+    //   console.log("results", results);
+    //   if (!results || !results[0]) {
+    //     return;
+    //   }
+    //   if (!site_has_ton_connect(results[0])) {
+    //     return;
+    //   }
+    //   let domain = extractDomain(tab.url);
+    //   if (!is_whitelisted_domain(domain)) {
+    //     alert("This site is not whitelisted for TON Connect usage");
+    //   }
+    // });
   }
 });
